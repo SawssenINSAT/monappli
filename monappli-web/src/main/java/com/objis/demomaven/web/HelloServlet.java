1ef9d04d6af40f4733a56a2349a7104bc111399d
SawssenINSAT <sawsseninsat@gmail.com>
2013-10-16 23:06:14 +0200

    'm ines this is my first commit

---
+++ monappli-web/src/main/java/com/objis/demomaven/web/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.objis.demomaven.web;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet 
+extends javax.servlet.http.HttpServlet 
+/*
+ * Premier commentaire javaDoc
+ */
+
+implements javax.servlet.Servlet {
+ protected void doGet(HttpServletRequest request, 
+ HttpServletResponse response) 
+ throws ServletException, IOException {
+   response.setContentType("text/html");
+   PrintWriter out = response.getWriter();
+   out.println("Projet Maven : Servlet Lancée");
+   out.flush();
+   out.close();
+    
+ }                                  
+}
